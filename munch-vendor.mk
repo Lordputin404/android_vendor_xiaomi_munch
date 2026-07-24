6a1e9684742eb990db761a373c0e4ab67db8139d
HDzungx <hdzungx@gmail.com>
2026-07-24 21:38:47 +0700

    munch: Build vendor.xiaomi.hardware.touchfeature@1.0 from source
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- munch-vendor.mk
+++ munch-vendor.mk
@@ -376,7 +376,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vpp@1.1 \
     vendor.qti.hardware.vpp@1.2 \
     vendor.qti.hardware.vpp@1.3 \
-    vendor.xiaomi.hardware.touchfeature@1.0 \
     btaudio_offload_if \
     audio.primary.kona \
     sound_trigger.primary.kona \
